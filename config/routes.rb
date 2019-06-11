2ab16a0e44013a6f73b08e3af4cd503a0276e0da
yuto040308 <yuto.ge6@gmail.com>
2019-06-11 19:39:58 +0900

    admin.html index.html show.html maked

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :destroy, :update]
   resources :goods, only: [:index, :show]
-  resources :cart, only: [:index, :create, :update, :destroy]
+  resources :carts, only: [:index, :create, :update, :destroy]
   resources :orders, only: [:index, :create, :show, :destroy, :new]
 
   get "/" => "home#top"
