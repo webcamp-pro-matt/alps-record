2ee44d15db8e24ca49f71c09c31e83e8aaeb2f86
takaokiku1211 <iphone4s.day07@gmail.com>
2019-06-26 19:28:52 +0900

    cart,good,music_titleバリデーション追加

--- app/models/good.rb
+++ app/models/good.rb
@@ -6,4 +6,7 @@ class Good < ApplicationRecord
 
 	accepts_nested_attributes_for :music_titles, allow_destroy: true
 	accepts_nested_attributes_for :carts, :orders_children
+
+	validates :price, presence: true, format: { with: /\A[0-9]+\Z/ }
+	validates :artist, :goods_title, :type, :label, :stock, presence: true
 end
